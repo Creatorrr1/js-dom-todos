dba188078240f2904e17e1c1f599ac56da2ecdbe
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-04-27 17:56:36 +0100

    todo list completed with critrea and extended critrea met

--- index.js
+++ index.js
@@ -15,6 +15,61 @@ function renderTodoListList() {
           li.className = "completed";
         }
         list.append(li);
+        // so if the todo is completed but is classed as completed, there should be a button that can modify its status
+        // if()
+        // const li = document.createElement('li')
+        // li.innerText = todo.completed
+        // ul.append(li)
+
+        const completeButton = document.createElement("input");
+        completeButton.setAttribute("type", "button");
+        completeButton.setAttribute("value", "✅");
+        li.append(completeButton);
+
+        const deleteButton = document.createElement("input");
+        deleteButton.setAttribute("type", "button");
+        deleteButton.setAttribute("value", "❌");
+        li.append(deleteButton);
+
+        completeButton.addEventListener("click", (completedEvent) => {
+          completedEvent.preventDefault();
+          function updateStatus() {
+            console.log("here");
+            // not sure what to do the url
+            fetch(`http://localhost:3000/todos/${todo.id}`, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ completed: true }),
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data);
+                // get the current list item (li)
+                // set the attribute of the css class completed
+                if (todo.completed) {
+                    li.className = "complete";
+                }
+              });
+          }
+          updateStatus();
+        });
+
+        deleteButton.addEventListener("click", (deleteEvent) => {
+        //   deleteEvent.preventDefault();
+
+          function deleteTodo() {
+            if (todo.completed) {
+              //   delete by targeting the id
+              fetch(`http://localhost:3000/todos/${todo.id}`, {
+                method: "DELETE",
+              });
+              list.remove()
+            }
+          }
+          deleteTodo();
+        });
       });
     });
 }
@@ -34,8 +89,7 @@ form.addEventListener("submit", (event) => {
   // make a POST request to the server with the data
   fetch("http://localhost:3000/todos", opts)
     .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
+    .then(() => {
       renderTodoListList();
     });
 });
